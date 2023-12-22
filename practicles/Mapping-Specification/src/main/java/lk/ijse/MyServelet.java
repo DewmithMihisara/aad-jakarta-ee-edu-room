4905ae1ce36584a093a10c49398e5d829d8825df
Dewmith Mihisara <mihisaralokuhewage@gmail.com>
2023-12-22 13:42:24 +0530

    done

---
+++ practicles/Mapping-Specification/src/main/java/lk/ijse/MyServelet.java
@@ -0,0 +1,21 @@
+package lk.ijse;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "MyServelet", value = "/test")
+public class MyServelet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        PrintWriter out=response.getWriter();
+        out.println("<h1>Hello World</h1>");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
