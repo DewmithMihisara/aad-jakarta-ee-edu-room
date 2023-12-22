0318f323ef4d3aa1ec69480b42f3e7eb78527ac2
Dewmith Mihisara <mihisaralokuhewage@gmail.com>
2023-12-22 14:08:08 +0530

    wildcard mapping

--- practicles/Mapping-Specification/src/main/java/lk/ijse/Servlet2.java
+++ practicles/Mapping-Specification/src/main/java/lk/ijse/Servlet2.java
@@ -3,16 +3,14 @@ package lk.ijse;
 import jakarta.servlet.*;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
-import java.io.IOException;
-import java.io.PrintWriter;
 
-@WebServlet(name = "MyServelet", value = "/test")
-public class MyServelet extends HttpServlet {
+import java.io.IOException;
+
+@WebServlet(name = "Servlet2", value = "/*")
+public class Servlet2 extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        PrintWriter out=response.getWriter();
-        out.println("<h1>Hello World</h1>");
-
+        response.getWriter().println("<h1>Servelet 2</h1>");
     }
 
     @Override
