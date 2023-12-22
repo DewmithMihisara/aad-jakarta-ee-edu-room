f2fe5f70f893e4192b768b55b60aec42ed086ffc
Dewmith Mihisara <mihisaralokuhewage@gmail.com>
2023-12-22 14:29:29 +0530

    extension mapping done

--- practicles/Mapping-Specification/src/main/java/lk/ijse/ServletExtensionMapping.java
+++ practicles/Mapping-Specification/src/main/java/lk/ijse/ServletExtensionMapping.java
@@ -6,11 +6,11 @@ import jakarta.servlet.annotation.*;
 
 import java.io.IOException;
 
-@WebServlet(name = "ServletExactMapping", value = "/ServletExactMapping")
-public class ServletExactMapping extends HttpServlet {
+@WebServlet(name = "Servlet2", value = "*.html")
+public class ServletExtensionMapping extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        response.getWriter().println("<h1>Servelet 3</h1>");
     }
 
     @Override
