e49ea8caea38dd4986aef699a237ba64ff8eccbc
Dewmith Mihisara <mihisaralokuhewage@gmail.com>
2024-01-12 01:44:36 +0530

    done hw patch

--- practicles/Test-Web/src/main/java/lk/ijse/management/CustomerServlet.java
+++ practicles/Test-Web/src/main/java/lk/ijse/management/CustomerServlet.java
@@ -132,7 +132,6 @@ public class CustomerServlet extends HttpServlet {
     @Override
     protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         Connection connection = null;
-
         JsonReader reader = Json.createReader(req.getReader());
         JsonObject jsonObject = reader.readObject();
 
