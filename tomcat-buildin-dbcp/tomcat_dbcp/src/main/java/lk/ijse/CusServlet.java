cdcd6ed8aac70ba0bfd401587080dd7d9b5fc13f
Dewmith Mihisara <mihisaralokuhewage@gmail.com>
2024-01-26 10:06:14 +0530

    test

--- tomcat-buildin-dbcp/tomcat_dbcp/src/main/java/lk/ijse/CusServlet.java
+++ tomcat-buildin-dbcp/tomcat_dbcp/src/main/java/lk/ijse/CusServlet.java
@@ -6,12 +6,30 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
 
 @WebServlet(name = "CusServlet", urlPatterns = "/customer")
 public class CusServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        
+    }
+    @Override
+    public void init() throws ServletException {
+        try {
+            InitialContext  initContext = new InitialContext();
+            DataSource pool=(DataSource) initContext.lookup("java:comp/env/jdbc/pos");
+
+            Connection connection = pool.getConnection();
+
+            System.out.println(connection);
+        } catch (NamingException | SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
